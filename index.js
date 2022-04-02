f624a42c25e0ee4ab08d4757745a5b89bd2150bc
= <antonia.balluais@mail-ecv.fr>
2022-04-02 18:48:06 +0200

    add routes home

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
+const homeRouter = require('./routes/home')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/home',homeRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

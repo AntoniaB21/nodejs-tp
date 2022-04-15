ba5d3cbf4cb410f8b15737ead3e2d6fb494ca893
maryam223 <maryam.traore@gmail.com>
2022-04-15 23:48:57 +0200

    Update index.js with axios

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const episodeRouter = require('./routes/episodes');
 const usersRouter = require('./routes/users');
 
+const axios = require("axios").create({baseUrl: "https://api.tvmaze.com/"});
+
 app.use(bodyParser.json())
 app.use('/shows',showsRouter);
 app.use('/episodes',episodeRouter);
@@ -14,6 +16,32 @@ app.use('/users',usersRouter);
 app.get('/', (req, res) => {
     res.json({'message':'API is running fine!'})
 })
+
+app.get("/axios-shows", (req, res) => {
+	axios({
+		url: "show",
+		method: "get",
+	})
+		.then(response => {
+			res.status(200).json(response.data);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: err });
+		});
+});
+
+app.get("/axios-people", (req, res) => {
+	axios({
+		url: "people",
+		method: "get",
+	})
+		.then(response => {
+			res.status(200).json(response.data);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: err });
+		});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
